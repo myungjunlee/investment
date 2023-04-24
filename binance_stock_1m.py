c7dd046f91d98e090a8527844dbb193e4c8493d4
South Korean Lee <dlaudwns789@gmail.com>
2023-04-24 16:35:04 +0900

    Update binance_stock_1m.py

--- binance_stock_1m.py
+++ binance_stock_1m.py
@@ -23,9 +23,7 @@ def ohlcv_data():
             start_date = to_mstimestamp('2017-01-01 00:00:00')
         else:
             start_date = to_mstimestamp(str(input_date + datetime.timedelta(minutes=1)))
-        # end_date = to_mstimestamp('2017-12-31 23:59:59')
         ohlcv = binance.fetch_ohlcv('BTC/USDT', timeframe='1m', since=start_date, limit=1000)
-        # ohlcv = binance_usdm.fetch_ohlcv('BTC/USDT', timeframe='1m', since=start_date, limit=1000, params={'endTime':end_date})
 
         df = pd.DataFrame(ohlcv, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
         df['Date'] = pd.to_datetime(df['Date'], unit='ms') + datetime.timedelta(hours=9)
@@ -46,5 +44,4 @@ def ohlcv_data():
     return final_df
 
 trading_df=ohlcv_data()
-print(trading_df)
 trading_df.to_sql("bitcoin1m", con, index=False, if_exists='replace')
